ea8087f0658be113f56714877f90abc117b88ea4
nowayrm <your.email@example.com>
2026-04-23 11:19:23 +0300

    some updates

--- app/types.ts
+++ app/types.ts
@@ -32,9 +32,9 @@ export const PLANS: Plan[] = SITE_PLANS
 
 export const OS_INSTALL_OPTIONS: OsInstallOption[] = [
   { id: 'ios', label: 'iOS', url: 'https://apps.apple.com/ua/app/happ-proxy-utility/id6504287215' },
-  { id: 'android', label: 'Android', url: 'https://play.google.com/store/apps/details?id=com.happproxy?' },
+  { id: 'android', label: 'Android', url: 'https://play.google.com/store/apps/details?id=com.happproxy' },
   { id: 'macos', label: 'macOS', url: 'https://apps.apple.com/ua/app/happ-proxy-utility/id6504287215' },
   { id: 'windows', label: 'Windows', url: 'https://github.com/Happ-proxy/happ-desktop/releases/latest/download/setup-Happ.x64.exe?' },
-  { id: 'android-tv', label: 'Android TV', url: 'https://play.google.com/store/apps/details?id=com.happproxy?' },
+  { id: 'android-tv', label: 'Android TV', url: 'https://play.google.com/store/apps/details?id=com.happproxy' },
   { id: 'apple-tv', label: 'Apple TV', url: 'https://apps.apple.com/ua/app/happ-proxy-utility-for-tv/id6748297274?' },
 ]
